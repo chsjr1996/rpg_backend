91a042132b19db4566796777ce5d69e59d70bf1e
Carlos Henrique <chsjr1996@gmail.com>
2022-12-17 17:00:58 -0300

    WIP: Add OpenSwoole
    Allow to restart Swoole app to apply code changes

--- src/SwooleServer.php
+++ src/SwooleServer.php
@@ -16,9 +16,22 @@ class SwooleServer
             echo "Swoole http server is started at http://0.0.0.0:9501\n";
         });
 
-        $server->on("request", function (Request $request, Response $response) {
+        $server->on("request", function (Request $request, Response $response) use ($server) {
+            $requestUri = data_get($request->server, '[request_uri]');
+            if ($requestUri === '/reload') {
+                $server->reload();
+
+                $html = file_get_contents(__DIR__ . '/Html/reload.html');
+                $response->header("Content-Type", "text/html");
+                $response->end($html);
+            }
+
             $response->header("Content-Type", "application/json");
-            $response->end(json_encode(Game::run()));
+            $response->end(json_encode([
+                'game' => Game::run(),
+                'request_uri' => $requestUri,
+                'server' => $request->server,
+            ]));
         });
 
         $server->start();
